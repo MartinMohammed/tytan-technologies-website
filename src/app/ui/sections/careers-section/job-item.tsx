194f463b727b8007c646dd7a36cf1653336266e6
Martin Mohammed <martinmohammed@MacBookAir.fritz.box>
2024-10-01 15:37:08 +0200

    wrap 'job-item' with Link component to be navigated to job href in _blank style.

--- src/app/ui/sections/careers-section/job-item.tsx
+++ src/app/ui/sections/careers-section/job-item.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "@/app/ui/sections/careers-section/job-item.module.css";
 import { IImage, IJob, TEXT_STYLES_ENUM } from "@/app/lib/definition";
 import Image from "next/image";
+import Link from "next/link";
 
 interface IJobItemProps {
   job: IJob;
@@ -10,37 +11,40 @@ interface IJobItemProps {
 
 function JobItem({ job, icon }: IJobItemProps) {
   return (
-    <div className={styles.job_item}>
-      <span
-        className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_title}`}
-      >
-        {job.title}
-      </span>
-      <span
-        className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_department}`}
-      >
-        {job.department}
-      </span>
-      <span
-        className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_product}`}
-      >
-        {job.product}
-      </span>
-      <div className={styles.job_item_right_section}>
+    <Link href={job.href} target="_blank">
+      <div className={styles.job_item}>
         <span
-          className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_location}`}
+          className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_title}`}
         >
-          {job.location}
+          {job.title}
         </span>
-        <Image
-          src={icon.src}
-          alt={icon.alt}
-          width={icon.width}
-          height={icon.height}
-          className={styles.arrow}
-        />
+        <span
+          className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_department}`}
+        >
+          {job.department}
+        </span>
+        <span
+          className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_product}`}
+        >
+          {job.product}
+        </span>
+        <div className={styles.job_item_right_section}>
+          <span
+            className={`${TEXT_STYLES_ENUM.SMALL_TEXT_STYLE} ${styles.job_location}`}
+          >
+            {job.location}
+          </span>
+
+          <Image
+            src={icon.src}
+            alt={icon.alt}
+            width={icon.width}
+            height={icon.height}
+            className={styles.arrow}
+          />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
